7bbd60fc5b22c71b58ff8284bf534bd420268d14
human.soft <human.soft@hotmail.com>
2025-01-17 16:38:26 +0700

    (oum) edit automate test-script on login-page

--- src/pages/login.page.js
+++ src/pages/login.page.js
@@ -3,15 +3,15 @@ import { removeTrailingSlash } from "../utils";
 
 export class LoginPage {
     baseURL = "https://hms.humansoft.co.th/auth/login";
-    loginSuccessURL = "https://hms.humansoft.co.th/welcome";
-    cssURL = "https://ccs.humansoft.co.th/auth/login";
 
     locatorCompany  = '#mat-select-0';
     locatorUsername = '[formcontrolname="email"]';
     locatorPassword = '[formcontrolname="password"]';
-    locatorButtonLogin = '[aria-label="LOG IN"]';
+    locatorLoginButton = '[aria-label="LOG IN"]';
     locatorAlertUsername = '#mat-error-3';
-    locatorAlertPassword = '#mat-error-4';
+    locatorAlertPassword = '#mat-error-4'; 
+    locatorLoginFailModal = '.swal2-popup.swal2-icon-error';
+    locateConfirmModalButton = '.swal2-confirm.swal2-styled';
 
     /**
     * @param {Page} page
@@ -25,40 +25,70 @@ export class LoginPage {
 
     async goto() {
         await this.page.goto(this.baseURL);
-    }
+    };
+    
+    /* Locator */
+
+    async locateUsername() {
+        return this.page.locator(this.locatorUsername);
+    };
+
+    async locatePassword() {
+        return this.page.locator(this.locatorPassword);
+    };
+    
+    async locateLoginButton() {
+        return this.page.locator(this.locatorLoginButton);
+    };
+    
+    async locateLoginFailModal() {
+        return this.page.locator(this.locatorLoginFailModal);
+    };
 
     /* Input */
+
     async clickSelectCompany(company) {
         await this.page.waitForSelector(this.locatorCompany);
         await this.page.locator(this.locatorCompany).click();
         await this.page.waitForSelector('.cdk-overlay-pane mat-option');
         await this.page.locator(`.cdk-overlay-pane .mat-option-text >> text=${company}`).click();
-    }
+    };
 
     async fillUsername(username) {
-        await this.page.locator(this.locatorUsername).fill(username);
-    }
+        await this.page.locator(this.locatorUsername).fill(username ?? '');
+    };
     
     async fillPassword(password) {
-        await this.page.locator(this.locatorPassword).fill(password);
-    }
+        await this.page.locator(this.locatorPassword).fill(password ?? '');
+    };
     
     async clickLogin() {
-        await this.page.locator(this.locatorButtonLogin).click();
-        await this.page.waitForURL(/welcome/);
-        const currUrl = this.page.url();
-        return currUrl;
-    }
+        await this.page.locator(this.locatorLoginButton).click();
+    };
 
     async clickInputUsername() {
         await this.page.locator(this.locatorUsername).click();
-    }
+    };
 
     async clickInputPassword() {
         await this.page.locator(this.locatorPassword).click();
-    }
+    };
+
+    async clickShowPassword() {
+        await this.page.waitForSelector('mat-icon', { state: 'visible' });
+        await this.page.locator('mat-icon', { hasText: 'visibility_off' });
+    };
+
+    async clickForgetPassword() {
+        await this.page.getByText('ลืมรหัสผ่าน').click();
+    };
+
+    async clickConfirmModal() {
+        await this.page.locator(this.locateConfirmModalButton).click();
+    };    
 
     /* Get Data */
+
     async getSelectCompany() {
         try {
             return await this.page.locator(this.locatorCompany).textContent();
@@ -67,7 +97,7 @@ export class LoginPage {
         }
         
         return"";
-    }
+    };
     
     async getUsername() {
         try {
@@ -77,17 +107,17 @@ export class LoginPage {
         }
 
         return"";
-    }
+    };
 
     async getPassword() {
         try {
-            return await this.page.locator(this.locatorAlertPassword).inputValue();
+            return await this.page.locator(this.locatorPassword).inputValue();
         } catch (error) {
             console.log('getPassword (error): ', error)
         }
 
         return"";
-    }
+    };
 
     async getAlertUsername() {
         try {
@@ -97,7 +127,7 @@ export class LoginPage {
         }
   
         return"";
-    }
+    };
 
     async getAlertPassword() {
         try {
@@ -107,11 +137,40 @@ export class LoginPage {
         }
 
         return"";
-    }
+    };
+
+    async getAlertLoginFailModal() {
+        try {
+            return await this.page.locator(this.locatorLoginFailModal).textContent() || "";            
+        } catch (error) {
+            console.log('getAlertLoginFailModal (error): ', error)
+        }
+
+        return"";
+    };
+
+    async getWelcomeUrl() {
+        // const currentUrl = this.page.url();
+      
+        // // await this.page.waitForURL(url => url !== currentUrl, { timeout: 5000 });  
+        // // return this.page.url();;   
+        
+        // await this.page.waitForURL(/welcome/);
+        // await this.page.waitForURL(url => url !== currentUrl);  
+        // const newUrl = await this.page.url(); 
+        // return newUrl;
+
+        
+        const currentUrl = this.page.url();  // ดึง current URL เป็น string
+        // รอให้ URL เปลี่ยนแปลงและมีคำว่า "welcome"
+        await this.page.waitForURL(url => /welcome/.test(url.href) && url.href !== currentUrl, { timeout: 5000 });
+        const newUrl = await this.page.url(); 
+        return newUrl;
+    };
 
     /* Check and compare */
     async isValidUrl() {
         const url = removeTrailingSlash(this.page.url());
         return url === this.baseURL;
-    }
+    };
 }
--- src/tests/login.spec.js
+++ src/tests/login.spec.js
@@ -2,59 +2,109 @@ import { expect } from "@playwright/test";
 import { test } from "../pages/base";
 import { LoginPage } from "../pages/login.page";
 import { validUsers, problemUsers } from "../test-data/users";
+import { log } from "console";
 
 test.describe('Login Page', () => {
-
+    
     test.beforeEach(async ({ loginPage }) => {
         await loginPage.goto();
     });
-
-    test('TC_001: Choose company from select list', async ({ loginPage }) => {
-        await loginPage.clickSelectCompany('OumforTest');
-        expect(await loginPage.getSelectCompany()).not.toBeNull();
-        expect(await loginPage.getSelectCompany()).toBe('OumforTest');
-    });
-
-    test('TC_002: Filled Username', async ({ loginPage }) => {
-        await loginPage.fillUsername('6501');
-        expect(await loginPage.getUsername()).not.toBeNull();
-        expect(await loginPage.getUsername()).toBe('6501');
-    });
-
-    test('TC_003: Not fill Username', async ({ loginPage }) => {
-        await loginPage.fillUsername('');
-        expect(await loginPage.getUsername()).toBe('');
-
-        await loginPage.clickInputPassword();
-
-        const alertMessage = await loginPage.getAlertUsername();
-        expect(alertMessage).toContain('กรุณาตรวจสอบชื่อผู้ใช้');  
-    });
     
-    test('TC_004: Filled Password', async ({ loginPage }) => {
+    test('TC_001: Input fields without username and password', async ({ loginPage }) => {
+        const loginButton = await loginPage.locateLoginButton(); // ตรวจสอบว่าปุ่ม Login ถูก disable ตั้งแต่เริ่มต้น
+        expect(loginButton).toBeDisabled();
+    
+        await loginPage.clickSelectCompany('OumforTest');
+        expect(await loginPage.getSelectCompany('OumforTest'));
+    
+        await loginPage.fillUsername(null);
+        expect(await loginPage.getUsername()).toBe('');
+        
+        await loginPage.fillPassword(null);
+        expect(await loginPage.getPassword()).toBe('');
+    
+        expect(loginButton).toBeDisabled();
+    
+        // expect (await loginPage.page.url()).toBe('https://hms.humansoft.co.th/auth/login');
+    });
+
+    test('TC_002: Input fields without username', async ({ loginPage }) => {
+        const loginButton = await loginPage.locateLoginButton(); // ตรวจสอบว่าปุ่ม Login ถูก disable ตั้งแต่เริ่มต้น
+        expect(loginButton).toBeDisabled();
+
+        await loginPage.clickSelectCompany('OumforTest');
+        expect(await loginPage.getSelectCompany('OumforTest'));
+    
+        await loginPage.fillUsername(null);
+        expect(await loginPage.getUsername()).toBe('');
+        
         await loginPage.fillPassword('1234');
         expect(await loginPage.getPassword()).not.toBeNull();
         expect(await loginPage.getPassword()).toBe('1234');
-    });
-    
-    test.only('TC_005: Not Fill Password', async ({ loginPage }) => {
-        await loginPage.clickInputPassword();
-        // await loginPage.fillPassword('');
-        // expect(await loginPage.getPassword()).toBe('');
-        await loginPage.clickInputUsername();
+        
+        const alertUsernameMessage = await loginPage.getAlertUsername();
+        expect(alertUsernameMessage).toContain('กรุณาตรวจสอบชื่อผู้ใช้');  
 
-        const alertMessage = await loginPage.getAlertPassword();
-        expect(alertMessage).toContain('กรุณาตรวจสอบรหัสผ่าน');  
+        expect(loginButton).toBeDisabled();
+
+        // expect (await loginPage.page.url()).toBe('https://hms.humansoft.co.th/auth/login');
     });
 
-    test('TC_006: Click show password button', async ({ loginPage }) => {
-    
-    });
-
-    test('TC_007: Selected and filled company, username and password data to login', async ({ loginPage }) => {
+    test('TC_003: Input fields without password', async ({ loginPage }) => {
+        const loginButton = await loginPage.locateLoginButton(); // ตรวจสอบว่าปุ่ม Login ถูก disable ตั้งแต่เริ่มต้น
+        expect(loginButton).toBeDisabled();
+        
         await loginPage.clickSelectCompany('OumforTest');
         expect(await loginPage.getSelectCompany('OumforTest'));
+        
+        await loginPage.fillPassword(null);
+        expect(await loginPage.getPassword()).toBe('');
+        
+        await loginPage.fillUsername('6501');
+        expect(await loginPage.getUsername()).not.toBeNull();
+        expect(await loginPage.getUsername()).toBe('6501');
 
+        const alertPasswordMessage = await loginPage.getAlertPassword();
+        expect(alertPasswordMessage).toContain('กรุณาตรวจสอบรหัสผ่าน');  
+
+        expect(loginButton).toBeDisabled();
+    
+        // expect (await loginPage.page.url()).toBe('https://hms.humansoft.co.th/auth/login');
+    });
+    
+    test('TC_004: Login fails with an error message when using invalid credentials', async ({ loginPage }) => {
+        const loginButton = await loginPage.locateLoginButton(); // ตรวจสอบว่าปุ่ม Login ถูก disable ตั้งแต่เริ่มต้น
+        // expect(loginButton).toBeDisabled();
+        
+        await loginPage.clickSelectCompany('OumforTest');
+        expect(await loginPage.getSelectCompany('OumforTest'));
+        
+        await loginPage.fillUsername('6501');
+        expect(await loginPage.getUsername()).not.toBeNull();
+        expect(await loginPage.getUsername()).toBe('6501');
+        
+        await loginPage.fillPassword('5678');
+        expect(await loginPage.getPassword()).not.toBeNull();
+        expect(await loginPage.getPassword()).toBe('5678');
+
+        expect(loginButton).not.toBeDisabled();
+
+        await loginPage.clickLogin();
+
+        expect(await loginPage.locateLoginFailModal()).toBeVisible();
+
+        const alertLoginFailMessage = await loginPage.getAlertLoginFailModal();
+        expect(alertLoginFailMessage).toContain('ชื่อผู้ใช้ หรือ รหัสผ่าน ไม่ถูกต้อง');
+
+        await loginPage.clickConfirmModal();
+        
+        // expect (await loginPage.page.url()).toBe('https://hms.humansoft.co.th/auth/login');
+    });
+
+    test('TC_005: Login successfully with valid credentials', async ({ loginPage }) => {
+        await loginPage.clickSelectCompany('OumforTest');
+        expect(await loginPage.getSelectCompany('OumforTest'));
+        
         await loginPage.fillUsername('6501');
         expect(await loginPage.getUsername()).not.toBeNull();
         expect(await loginPage.getUsername()).toBe('6501');
@@ -62,36 +112,68 @@ test.describe('Login Page', () => {
         await loginPage.fillPassword('1234');
         expect(await loginPage.getPassword()).not.toBeNull();
         expect(await loginPage.getPassword()).toBe('1234');
-
+        
         await loginPage.clickLogin();
+        await loginPage.getWelcomeUrl();
         expect (await loginPage.page.url()).toBe('https://hms.humansoft.co.th/welcome');
     });
+    
+    test.only('TC_006: Click show password', async ({ loginPage }) => {
+        await loginPage.fillPassword('1234');
+        expect(await loginPage.getPassword()).not.toBeNull();
+        expect(await loginPage.getPassword()).toBe('1234');
+        
+        // await loginPage.clickShowPassword();
+        // const passwordInput = await loginPage.clickInputPassword();
+        // expect(await expect(passwordInput).toHaveAttribute('type', 'text'));
 
-    test('TC_008: Choose company from select list', async ({ loginPage }) => {
-        await loginPage.clickSelectCompany('OumforTest');
-        expect(await loginPage.getSelectCompany()).not.toBeNull();
-        expect(await loginPage.getSelectCompany()).toBe('OumforTest');
+        // ดึง input และปุ่มไอคอน show/hide
+        const passwordInput = await loginPage.getPassword();
+        const toggleButton = await loginPage.clickShowPassword();
+
+        // ตรวจสอบว่าเริ่มต้น type เป็น "password"
+        await expect(passwordInput).toHaveAttribute('type', 'password');
+
+        // คลิกปุ่ม Show/Hide Password
+        await toggleButton.click();
+
+        // ตรวจสอบว่า type เปลี่ยนเป็น "text"
+        await expect(passwordInput).toHaveAttribute('type', 'text');
+
+        // คลิกอีกครั้งเพื่อซ่อนรหัสผ่าน
+        await toggleButton.click();
+
+        // ตรวจสอบว่า type กลับเป็น "password"
+        await expect(passwordInput).toHaveAttribute('type', 'password');
     });
+    
+    // test('TC_006: Not Fill Password', async ({ loginPage }) => {
+    //     await loginPage.clickSelectCompany('OumforTest');
+    //     expect(await loginPage.getSelectCompany()).not.toBeNull();
+    //     expect(await loginPage.getSelectCompany()).toBe('OumforTest');
 
+    //     await loginPage.fillUsername('6501');
+    //     expect(await loginPage.getUsername()).not.toBeNull();
+    //     expect(await loginPage.getUsername()).toBe('6501');
+        
+    //     await loginPage.fillPassword('');
+    //     expect(await loginPage.getPassword()).toBe('');
+    //     await loginPage.clickInputUsername();
+        
+    //     const alertMessage = await loginPage.getAlertPassword();
+    //     expect(alertMessage).toContain('กรุณาตรวจสอบรหัสผ่าน');  
+    // });
+
+    // test('TC_001: Choose company from select list', async ({ loginPage }) => {
+        //     await loginPage.clickSelectCompany('OumforTest');
+        //     expect(await loginPage.getSelectCompany()).not.toBeNull();
+    //     expect(await loginPage.getSelectCompany()).toBe('OumforTest');
+    // });
+    
     // test('TC_002: Filled Username', async ({ loginPage }) => {
     //     await loginPage.fillUsername('6501');
     //     expect(await loginPage.getUsername()).not.toBeNull();
     //     expect(await loginPage.getUsername()).toBe('6501');
-    // });
-
-    // test.only('TC_003: Not fill Username', async ({ loginPage }) => {
-    //     await loginPage.fillUsername('');
-    //     expect(await loginPage.getUsername()).toBe('');
-
-    //     await loginPage.fillPassword('1234');
-    //     // expect(await loginPage.getPassword()).toBe('1234');
-    //     expect(await loginPage.getPassword()).not.toBeNull();
-
-    //     const message = await loginPage.getAlertUsername();
-    //     expect(message).toContain('กรุณาตรวจสอบชื่อผู้ใช้');  
-        
-    //     await loginPage.clickLogin();
-    //     expect(await loginPage.clickLogin()).toBeDisabled();
     // });
     
     // test('TC_004: Filled Password', async ({ loginPage }) => {
@@ -99,6 +181,32 @@ test.describe('Login Page', () => {
     //     expect(await loginPage.getPassword()).not.toBeNull();
     //     expect(await loginPage.getPassword()).toBe('1234');
     // });
+    
+    
+
+
+
+    test('TC_010 Selected company without username and password data to login', async ({ loginPage }) => {
+        await loginPage.clickSelectCompany('OumforTest');
+        expect(await loginPage.getSelectCompany('OumforTest'));
+
+        await loginPage.fillUsername('');
+        expect(await loginPage.getUsername()).toBe('');
+        
+        await loginPage.fillPassword('');
+        expect(await loginPage.getPassword()).toBe('');
+
+        // await loginPage.clickLogin();
+        // const isDisabled = (await loginPage.clickLogin()).isDisabled();
+        // expect (isDisabled).toBe(true);
+
+        expect (await loginPage.page.url()).toBe('https://hms.humansoft.co.th/auth/login');
+    });
+    
+    test('TC_011: Click forget password', async ({ loginPage }) => {
+        await loginPage.clickForgetPassword();
+        await page.locator('div').filter({ hasText: /^คืนค่ารหัสผ่าน$/ }).first().click();
+    });
     
     // test('TC_005: Not Filled Password', async ({ loginPage }) => {
     //     await loginPage.fillPassword('');
